c43fcc8d24eeba540973cc091c05f905282f01ee
Teddy Robinson <contactteddyrobinson@gmail.com>
2023-09-14 10:41:59 +0200

    first dto set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { InstModule } from './inst/inst.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, InstModule],
+  imports: [AuthModule, UserModule, InstModule, PrismaModule],
 })
 export class AppModule {}
