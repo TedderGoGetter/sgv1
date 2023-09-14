d0ce595f17aaa7a15e335e693f8e73537bfd9af5
Teddy Robinson <contactteddyrobinson@gmail.com>
2023-09-14 12:37:28 +0200

    basic validation and hashing working

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable()
-
 export class AuthService {
-    constructor(private prisma: PrismaService) {
-
-    }
-    signup() {
-        return { msg: 'You have signed up!'}
+    constructor(private prisma: PrismaService) {}
+    async signup(dto: AuthDto) {
+        //we want to generate the password hash
+        const hash = await argon.hash(dto.password);
+        //add the user to the db
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+                userName: dto.email //data couldn't exist without userName for some reason. Until I fix it I'll set userName to the email.
+            },
+        })
+        //return the saved user
+        return user;
     }
 
     signin() {
