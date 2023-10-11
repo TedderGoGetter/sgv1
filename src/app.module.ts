4af2a089a3648db9cb916dd580b90c6a0a247ea6
Teddy Robinson <contactteddyrobinson@gmail.com>
2023-10-11 14:05:39 +0200

    added config security

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { InstModule } from './inst/inst.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, InstModule, PrismaModule],
+  imports: [AuthModule, UserModule, InstModule, PrismaModule, ConfigModule.forRoot({
+    isGlobal: true,
+  })],
 })
 export class AppModule {}
