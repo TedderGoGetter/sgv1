23b456c49b97c2e435a74b70b96d00a976c73ed6
Teddy Robinson <contactteddyrobinson@gmail.com>
2023-10-21 10:15:33 +0200

    logic for test db added

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,24 @@
 import {Test} from '@nestjs/testing'
 import { AppModule } from '../src/app.module'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
 
 describe ('App e2e', () => {
-  beforeAll(async () => {
+  let app: INestApplication 
+  beforeAll(async () => {  //starting logic
+
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule]
     }).compile()
+
+    // this is where you simulate main.ts:
+    app = moduleRef.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+    }));
+    await app.init()
   })
-  it.todo('should pass')
+
+  afterAll(() => app.close) //ending logic
+
   it.todo('should pass')
 })
